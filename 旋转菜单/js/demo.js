c5f9f92c513a4b8a84a1c3217031f78a7ca02e72
yuan <m13072866230_1@163.com>
2020-05-14 10:13:32 +0800

    加入旋转菜单和日出日落

---
+++ 旋转菜单/js/demo.js
@@ -0,0 +1,21 @@
+function rotateZ(li,deg) {
+	$(li).css({transform:'rotateZ('+deg+'deg)'}).find('label').css({transform:'rotateZ('+-deg+'deg)'})
+}
+
+function changeOptions(ele) {
+	var $selector = $(ele).parent();
+	$selector.toggleClass('open');
+	var $li = $selector.find('li');
+	var deg = 360/$li.length;
+	
+	for(var i = 0; i < $li.length; i++) {
+		var d = i * deg;
+		$selector.hasClass('open') ? rotateZ($li[i],d) : rotateZ($li[i],-360); 
+	}
+}
+
+$('button').click(function(){
+	changeOptions(this);
+})
+
+setTimeout(function() { changeOptions('.selector button'); }, 100);
